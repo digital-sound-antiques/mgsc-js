92aa8f29d21d3cb98020159315230b4effc4b4e2
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2016-01-13 14:33:05 +0900

    Initial Commit.

---
+++ src/bridge.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <sstream>
+
+#include "mgsc.h"
+
+static void normalize_newline(std::istream& is, std::stringstream &ss) {
+  std::string buf;
+  while(getline(is,buf)) {
+    ss << buf << "\r\n";
+  }
+}
+
+extern "C" size_t MGSC_compile(char *mml, char *mgs, char *mes) {
+
+	std::stringstream is;
+	is << mml;
+	std::stringstream ss;
+	normalize_newline(is,ss);
+
+	MGSC mgsc;
+	std::stringstream log;
+	mgsc.SetConsole(&std::cin,&log);
+	std::stringstream os;
+	mgsc.Compile(&ss,&os,1);
+
+	strcpy(mes, log.str().c_str());
+
+	size_t size = os.str().size();
+	if(0<size) {
+		memcpy(mgs, os.str().c_str(), size);
+	}
+	return size;
+
+}
---
+++ src/main.js
@@ -0,0 +1,22 @@
+var MGSC = (function(){
+	"use strict"
+	var MGSC = function() {};
+
+	MGSC.compile = function(mml) {			
+
+		var ptr = Module._malloc(32768);
+		var log = Module._malloc(32768);
+		var size = Module.ccall('MGSC_compile', 'number', ['string','number'], [mml,ptr,log]);
+
+		var mgs = new Uint8Array(size);
+		mgs.set(new Uint8Array(Module.HEAPU8.buffer,ptr,size));
+
+		Module._free(ptr);
+		Module._free(log);
+		return {
+			mgs:mgs,
+			log:AsciiToString(log)
+		};
+	};
+	return MGSC;
+}());
