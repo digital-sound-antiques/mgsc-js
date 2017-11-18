deaa8642584688a49ff8646c4b67aaa6c6749b90
okaxaki <okaxaki@users.noreply.github.com>
2017-11-19 00:32:25 +0900

    Add options and document.

--- README.md
+++ README.md
@@ -1,29 +1,64 @@
 # mgsc-js
 
-mgsc-js is a Javascript version of [MGSC](https://github.com/digital-sound-antiques/mgsc) (MML to MGS compiler) build with [Emscripten](https://github.com/kripken/emscripten).
+[mgsc-js] is a JavaScript version of [MGSC] (MML to MGS compiler for [MGSDRV]) build with [Emscripten]
 
-# How to Install
+[mgsc-js]: https://github.com/digital-sound-antiques/mgsc-js
+[MGSC]: https://github.com/digital-sound-antiques/mgsc
+[MGSDRV]: http://www.gigamix.jp/mgsdrv/
+[Emscripten]: https://github.com/kripken/emscripten
 
+Repository: [mgsc-js GitHub](https://github.com/digital-sound-antiques/mgsc-js)
+
+# Install as Command-Line Tool
+```sh
+$ npm install -g mgsc-js
 ```
-npm install -g mgsc-js
+
+# How to Use Command
+```sh
+$ mgsc-js [options] source
+```
+where the `source` is MGSC's MML source text.
+
+## Options
+```sh
+$ mgsc-js
+Usage: mgsc [options] source
+[options]
+  -o, --output FILE   : Specify the output filename.
+  -e, --encode ENCODE : Specify the input file encoding [default:AUTO].
+                        Available encodings: UTF16, UTF16BE, UTF16LE, 
+                        JIS, UTF8, EUCJP, SJIS
+  -t                  : Show track memory usage.
+  -h, --help          : Show this help.
 ```
 
-# How to Use
+# Install and Use as Library
+Install the mgsc-js as a node_module.
+```sh
+$ npm install --save mgsc-js
 ```
-mgsc-js filename [-o output]
+Then,
+```javascript
+var FS = require('fs');
+var MGSC = require('mgsc-js');
+var result = MGSC.compile('#opll_mode 0\n#tempo 120\n9 v13@0o4cdefgab>c\n');
+   
+if (!result.success) {
+  console.log(result.errorInfo.message + ' in ' + result.errorInfo.lineNumber);
+  console.log('>> ' + result.errorInfo.lineText + '\n');
+  process.exit(1);
+}
+
+FS.writeFileSync('result.mgs', new Buffer(result.mgs), "binary");
 ```
-where the `filename` is MGSC's MML source text.
 
-- If `-o` option is surpressed, the command generates `[filename].mgs`.
-
-# How to checkout & build
-
+# How to build Project
 Prerequesties: node.js, `cmake` and `emcmake` are required to build the project.
-
+```sh
+$ git clone --recursive https://github.com/digital-sound-antiques/mgsc-js.git
+$ cd mgsc-js
+$ npm install
+$ npm run build 
+$ npm run jsdoc # for document
 ```
-git clone --recursive https://github.com/digital-sound-antiques/mgsc-js.git
-cd mgsc-js
-npm install
-npm run build
-```
-
--- bin/cmd.js
+++ bin/cmd.js
@@ -1,48 +1,69 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const mgsc = require('../src/entry');
+const FS = require('fs');
+const Path = require('path');
+const MGSC = require('../src/entry');
+const Encoding = require('encoding-japanese');
 
 function printUsageAndExit(code) {
-	console.log(
-		'Usage: mgsc [options] mmlfile\n' + 
-		'\n' +
-		'  -o, --output FILE : Specify output filename.\n' +
-		'  -t                : Show the original mgsc message.\n' +
-		'  -h                : Print this help.\n');
-	process.exit(code);
+  console.log(
+    'Usage: mgsc [options] source\n' + 
+    '\n' +
+    '[options]\n' + 
+    '  -o, --output FILE   : Specify the output filename.\n' +
+    '  -e, --encode ENCODE : Specify the input file encoding [default:AUTO].\n' +
+    '                        Available encodings: UTF16, UTF16BE, UTF16LE, \n' +
+    '                        JIS, UTF8, EUCJP, SJIS\n' +
+    '  -t                  : Show track memory usage.\n' +
+    '  -h, --help          : Show this help.\n');
+  process.exit(code);
 }
 
 var argv = require('minimist')(process.argv.slice(2), {
-	string:['output'],
-	boolean:['t'],
-	alias:{'o':'output', 'T':'t'},
-	unknown:function(arg) {	
-		if (arg.charAt(0) !== '-') {
-			return true;
-		}
-		if (arg === '-h' || arg === '--help') {
-			printUsageAndExit(0);
-		}
-		printUsageAndExit(1);
-	}
+  string:['output','enc'],
+  boolean:['t'],
+  alias:{'o':'output', 'T':'t', 'e':'encode'},
+  unknown:function(arg) { 
+    if (arg.charAt(0) !== '-') {
+      return true;
+    }
+    if (arg === '-h' || arg === '--help') {
+      printUsageAndExit(0);
+    }
+    printUsageAndExit(1);
+  }
 });
 
 if (argv._.length == 0) {
-	printUsageAndExit(0);
+  printUsageAndExit(0);
 }
 
-var input = argv._[0];
-var output = argv.output || "./" + path.parse(input).name + ".mgs";
+const input = argv._[0];
+const name = Path.parse(input).name;
+const isUpperCaseName = name.match(/^[A-Z0-9_\-]+$/) != null;
+const ext = isUpperCaseName ? '.MGS' : '.mgs';
+const output = argv.output || name + ext;
+const buf = FS.readFileSync(input);
 
-var mml = fs.readFileSync(input, 'utf-8');
-var result = mgsc.compile(mml);
-
-if (argv.t) {
-  console.log(result.message);
-}
-
-fs.writeFileSync(output, new Buffer(result.mgs), "binary", function(err){
-	console.log(err);
+const source = Encoding.convert(buf, { 
+  from: (argv.encode || 'AUTO').toUpperCase(), 
+  to: 'UNICODE', 
+  type: 'string',
 });
+
+const result = MGSC.compile(source);
+console.log(result.bannerText);
+
+if (!result.success) {
+  console.log(result.errorInfo.message + ' in ' + result.errorInfo.lineNumber);
+  console.log('>> ' + result.errorInfo.lineText + '\n');
+  process.exit(1);
+}
+
+console.log('Compile complete');
+if (argv.t && result.trackInfoText != null) {
+  console.log(result.trackInfoText);
+}
+
+FS.writeFileSync(output, new Buffer(result.mgs), "binary");
+console.log('Save complete: ' + output);
